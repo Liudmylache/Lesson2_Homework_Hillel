eff17bb04a75b65dc8bbdc181d6b4a2fbb8b4820
Lucy <lucydenisenko@gmail.com>
2022-06-30 14:17:08 -0700

    new methods added

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,32 @@ public class Main {
 
         MyArrayList list = new MyArrayList();
         list.add(5);
-        list.add(1,6);
+        list.add(1,"6");
         list.add(5);
-        list.add(3,7);
+        list.add(3,"7");
+        list.set(1,"99");
+        list.add(4,"99");
+
 
 
         System.out.println(list);
         System.out.println(list.size());
-        System.out.println(list.contains(8));
-        System.out.println(list.contains(6));
+
+   //     list.remove("5");
+//        list.remove(9);
+ //      list.remove(2);
+//        list.remove(9);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.indexOf("8"));
+        System.out.println(list.subList(1,4));
+
+
+
+
+
+
+
 
 
 
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -75,7 +75,21 @@ public class MyArrayList implements List<Object> {
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        for (int i = 0; i<defaultArray.length; i++) {
+            if (defaultArray[i] == o) {
+                Object[] newArray = new Object[defaultArray.length - 1];
+                if (i == 0) {
+                    System.arraycopy(defaultArray, 0, newArray, 1, defaultArray.length - 1);
+                }
+                else {
+                    System.arraycopy(defaultArray, 0, newArray, 0, i);
+                    System.arraycopy(defaultArray, i + 1, newArray, i, defaultArray.length - (i + 1));
+                }
+                defaultArray = newArray;
+                sizeOfElements--;
+                return true;
+            } else return false;
+        } return true;
     }
 
     @Override
@@ -105,7 +119,9 @@ public class MyArrayList implements List<Object> {
 
     @Override
     public void clear() {
-
+        Object[] newArray = new Object[0];
+        defaultArray = newArray;
+        sizeOfElements = 0;
     }
 
     @Override
@@ -115,7 +131,8 @@ public class MyArrayList implements List<Object> {
 
     @Override
     public Object set(int index, Object element) {
-        return null;
+        defaultArray[index] = element;
+        return defaultArray[index];
     }
 
     @Override
@@ -130,17 +147,44 @@ public class MyArrayList implements List<Object> {
 
     @Override
     public Object remove(int index) {
+        if (defaultArray.length > index){
+            Object temp = defaultArray[index];
+            Object[] newArray = new Object[defaultArray.length - 1];
+            System.arraycopy(defaultArray, 0, newArray, 0, index);
+            System.arraycopy(defaultArray, index+1, newArray, index, defaultArray.length - (index+1));
+            defaultArray = newArray;
+            sizeOfElements--;
+            return temp;
+        }
+        if (index == 0){
+            Object temp = defaultArray[index];
+            Object[] newArray = new Object[defaultArray.length - 1];
+            System.arraycopy(defaultArray, 0, newArray, 1, defaultArray.length-1);
+            defaultArray = newArray;
+            sizeOfElements--;
+            return temp;
+        }
         return null;
     }
 
+
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i <defaultArray.length; i++){
+            if (defaultArray[i] == o) {
+                return i;
+            }
+        } return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int temp = -1;
+        for (int i = 0; i <defaultArray.length; i++){
+            if (defaultArray[i] == o) {
+                temp = i;
+            } continue;
+        } return temp;
     }
 
     @Override
@@ -155,7 +199,9 @@ public class MyArrayList implements List<Object> {
 
     @Override
     public List<Object> subList(int fromIndex, int toIndex) {
-        return null;
+        Object[] subList = new Object[toIndex];
+        System.arraycopy(defaultArray, fromIndex, subList,0, toIndex);
+        return Arrays.asList(subList);
     }
 
     @Override
